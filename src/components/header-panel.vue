1ac9acdec5ef23d27d53d72fc1006effc759c0f1
korstan <stas.korda@gmail.com>
2019-09-13 06:20:46 +0500

    added side menu, changed header bar

---
+++ src/components/header-panel.vue
@@ -0,0 +1,33 @@
+<template>
+  <header :class="$style.headerPanel">
+    <div :class="$style.logo">
+      Star Wars API
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderPanel',
+};
+</script>
+
+<style module>
+.headerPanel {
+  position: fixed;
+  top: 0;
+  height: 50px;
+  width: 100%;
+  background-color: black;
+  display: flex;
+  color: black;
+}
+.logo {
+  -webkit-text-stroke: 1px #fadb03;
+  font-size: 30pt;
+  font-weight: bold;
+  padding: 0 10px;
+  background-color: black;
+  cursor: default;
+}
+</style>
--- src/pages/app-home.vue
+++ src/pages/app-home.vue
@@ -1,20 +1,26 @@
 <template>
-  <div>
+  <div id="wrapper">
+    <HeaderPanel />
     <NavPanel />
     <router-view />
   </div>
 </template>
 
 <script>
+import HeaderPanel from '@/components/header-panel';
 import NavPanel from '@/components/nav-panel';
 
 export default {
   name: 'AppHome',
   components: {
-    NavPanel
+    HeaderPanel,
+    NavPanel,
   },
 };
 </script>
 
 <style>
+#wrapper{
+  padding-top: 50px;
+}
 </style>
