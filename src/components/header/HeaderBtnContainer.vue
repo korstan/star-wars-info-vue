e80b3a3e3c9c3a6d43d8e196e023cd6236e4947d
korstan <stas.korda@gmail.com>
2019-09-23 12:51:37 +0500

    menuBtn done, signOutBtn under construction

---
+++ src/components/header/HeaderBtnContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div 
+    :class="$style.btnContainer"
+    @click="$emit('headerBtnClick')"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderBtnContainer',
+}
+</script>
+
+<style module>
+  .btnContainer{
+    display: inline-block;
+    height: 100%;
+    cursor: pointer;
+  }
+</style>
